cc924ead4f08023ca7300a8c65e5fdb559eba864
RealGenius1 <sptho24@gmail.com>
2025-02-24 12:57:22 -0500

    Make real variables

--- MyApplication2/app/src/main/java/com/zybooks/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/zybooks/myapplication/MainActivity.kt
@@ -7,6 +7,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var height; // This will either be predefined or inputted by user (User input isn't hard)
+    private var mass; // This comes from selected phone (DATABASE)
+    private var dimensions[]; // 2 options: Either area variables, or dimensions (dimensions is easier)
+    private var vel; // Velocity: v = sqrt(19.6*h)
+    private var time; // This is for momentum stuff ; Time: t = sqrt(19.6*h)/deceleration
+    private var decel; // This is either just defined constant (607) OR it's a database (if we can find)
+    private var Force; // Force (technically pressure): mass * vel / (time * Area)
+    // MAX PRESSURE: 356 Newtons of Pressure
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,7 +38,6 @@ class MainActivity : AppCompatActivity() {
             // MAX PRESSURE: 356 Newtons of Pressure
             // If we have multiple areas (on face, corner, side) we can give more resolution
             // And the math is basically the same for all areas
-
 
          */
     }
