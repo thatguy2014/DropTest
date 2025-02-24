3416c95c3ba5189f08ade54d44b30db8c3f58a30
RealGenius1 <sptho24@gmail.com>
2025-02-24 13:26:21 -0500

    phone var and onClick()

--- MyApplication2/app/src/main/java/com/zybooks/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/zybooks/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private var height; // This will either be predefined or inputted by user (User input isn't hard)
+    private var Phone; // Selected phone
     private var mass; // This comes from selected phone (DATABASE)
     private var dimensions[]; // 2 options: Either area variables, or dimensions (dimensions is easier)
     private var vel; // Velocity: v = sqrt(19.6*h)
@@ -26,19 +27,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
-        /* BASELINE CODE FOR PHYSICS:
-            private var height; // This will either be predefined or inputted by user (User input isn't hard)
-            private var mass; // This comes from selected phone (DATABASE)
-            private var dimensions[]; // 2 options: Either area variables, or dimensions (dimensions is easier)
-            private var vel; // Velocity: v = sqrt(19.6*h)
-            private var time; // This is for momentum stuff ; Time: t = sqrt(19.6*h)/deceleration
-            private var decel; // This is either just defined constant (607) OR it's a database (if we can find)
-            private var Force; // Force (technically pressure): mass * vel / (time * Area)
-            // MAX PRESSURE: 356 Newtons of Pressure
-            // If we have multiple areas (on face, corner, side) we can give more resolution
-            // And the math is basically the same for all areas
-
-         */
+    //doing all the math
+    fun onClick(view : View){
+        //get height
+        //get phone
     }
 }
